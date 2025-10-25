fa23835225be2d4bf456b91b32ade5a70b0640e5
Mohit <bluesky5april2021@gmail.com>
2025-10-25 21:38:25 +0530

    Added: Example Paers

--- src/app/app.ts
+++ src/app/app.ts
@@ -30,7 +30,9 @@ interface Message{
 })
 
 export class App implements AfterViewChecked {
-  constructor(private scholar: SementicScholar, private http:HttpClient, public auth:Supabase) {}
+  constructor(private scholar: SementicScholar, private http:HttpClient, public auth:Supabase) {
+    this.periodic_inc_current_paper_index()
+  }
   text: WritableSignal<string> = signal('');
   finalQuestion: WritableSignal<string> = signal('');
 
@@ -283,5 +285,25 @@ export class App implements AfterViewChecked {
     if (this.bottom) {
       this.bottom.nativeElement.scrollIntoView({ behavior: 'smooth' });
     }
+  }
+
+  // ----------------------------------------------------------------
+  // List of Example Papers: ----------------------------------------
+  // ----------------------------------------------------------------
+  Current_paper_index = signal(0)
+  Paper_List = [
+    {"title":"Attention Is All You Need", "year":2017, "author":"Vaswani et al.", "link_to_pdf":"https://arxiv.org/pdf/1706.03762"},
+    {"title":"BERT: Pre‑training of Deep Bidirectional Transformers for Language Understanding", "year":2018, "author":"Devlin et al.", "link_to_pdf":"https://arxiv.org/pdf/1810.04805"},
+    {"title":"Exploring the Limits of Transfer Learning with a Unified Text‑to‑Text Transformer", "year":2019, "author":"Raffel et al.", "link_to_pdf":"https://arxiv.org/pdf/1910.10683"},
+    {"title":"An Image is Worth 16×16 Words: Transformers for Image Recognition at Scale", "year":2020, "author":"Dosovitskiy et al.", "link_to_pdf":"https://arxiv.org/pdf/2010.11929"},
+    {"title":"Linear attention is (maybe) all you need (to understand transformer optimization)", "year":2023, "author":"Ahn et al.", "link_to_pdf":"https://arxiv.org/pdf/2310.01082"}
+  ]
+
+  periodic_inc_current_paper_index(){
+    setInterval(()=>{
+      this.Current_paper_index.update(
+        (value:number) => (value+1)%this.Paper_List.length
+      )
+    }, 3000)
   }
 }
